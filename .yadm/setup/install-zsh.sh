63e03a0af12e3b19e1716b7a2da10d7db9b701f9
Stephen Cassidy <stephen.cassidy@arm.com>
2018-10-23 16:19:55 +0100

    Updating bootstrap

--- .yadm/setup/install-zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-if command -v zsh >/dev/null 2>&1; then
-  echo "Installing oh-my-zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null 2>&1i
-fi
