433b943594886d8beda4f35666fff284051851cf
stephencassidy-r7 <scassidy@rapid7.com>
2019-08-13 11:26:37 +0100

    More tidy up of shell scripts and helpers

--- .env.sh
+++ .env.sh
@@ -2,12 +2,14 @@
 
 autoload -Uz add-zsh-hook
 
+readonly RBENV_VERSION_FILE=".ruby-version"
+
 _version_file_readable () {
   [[ -f "${1}" ]]
 }
 
 _check_for_rbenv () {
-  version_file=".ruby-version"
+  local version_file=".ruby-version"
   if _version_file_readable "${version_file}"; then
     echo "===> Found ${version_file}, loading ..." 
     _load_rbenv
@@ -15,7 +17,7 @@ _check_for_rbenv () {
 }
 
 _check_for_pyenv () {
-  version_file=".python-version"
+  local version_file=".python-version"
   if _version_file_readable "${version_file}"; then
     echo "===> Found ${version_file}, loading ..." 
     _load_pyenv
@@ -23,7 +25,7 @@ _check_for_pyenv () {
 }
 
 _check_for_goenv () {
-  version_file=".go-version"
+  local version_file=".go-version"
   if _version_file_readable ${version_file}; then
     echo "===> Found ${version_file}, loading ..." 
     _load_goenv
@@ -31,7 +33,7 @@ _check_for_goenv () {
 }
 
 _check_for_jabba () {
-  version_file=".jabbarc"
+  local version_file=".jabbarc"
   if _version_file_readable ${version_file}; then
     _load_jabba
     jabba use
@@ -40,24 +42,20 @@ _check_for_jabba () {
 
 _load_rbenv () {
   source "${ZSH_PLUGINS}/rbenv/rbenv.plugin.zsh"
-  add-zsh-hook -d precmd _check_for_rbenv 
 }
 
 _load_pyenv () {
   source "${ZSH_PLUGINS}/pyenv/pyenv.plugin.zsh"
-  add-zsh-hook -d precmd _check_for_pyenv 
 }
 
 _load_goenv () {
   eval "$(goenv init -)"
   source "${ZSH_PLUGINS}/go/go.plugin.zsh"
   export PATH="${GOROOT}/bin:${GOPATH}/bin:${PATH}"
-  add-zsh-hook precmd _check_for_goenv
 }
 
 _load_jabba () {
   [ -s "/Users/scassidy/.jabba/jabba.sh" ] && source "/Users/scassidy/.jabba/jabba.sh"
-  add-zsh-hook precmd _check_for_jabba
 }
 
 add-zsh-hook precmd _check_for_rbenv 
